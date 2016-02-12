880f68d7699fd5c83b43decdef5c3981b52f17f5
Keerthan Mala <kmala@engineyard.com>
2016-02-12 11:54:11 -0700

    ref(postgres): move username and password to secrets

--- contrib/ci/test.sh
+++ contrib/ci/test.sh
@@ -5,6 +5,12 @@ set -x
 
 JOB=$(docker run -d $1)
 # wait for postgres to boot
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+mkdir -p tmp
+echo "testuser" > tmp/user
+echo "icanttellyou" > tmp/pass
+JOB=$(docker run -dv $CURRENT_DIR/tmp:/etc/secret-volume $1)
+# wait for postgres to boot
 sleep 10
 docker exec $JOB is_master
 docker rm -f $JOB
